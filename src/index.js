67c5d6a403a26523648190a35f496c2f37988a97
Nastya-Ivanova <tkacheva123@gmail.com>
2021-02-13 19:46:59 +0300

    added solutions to problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    return array.length ? array.sort( (item1, item2) => item1 - item2 )[0] : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    return array.length ? array.sort( (item1, item2) => item1 - item2 )[array.length-1] : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    let total = array.reduce(function(sum, item){
+        return sum + item;
+    }, 0);
+    return total ? total/array.length : 0;
 }
